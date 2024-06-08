522001a3c82b76cd0fbb15ab5c9c1d7173096018
Fabrice Gallet <74436347+fbgallet@users.noreply.github.com>
2024-06-08 18:27:45 +0200

    Replaced hotkey by command in command palette

--- extension.js
+++ extension.js
@@ -1,5 +1,5 @@
 (() => {
-  // customize hotkeys on line 19
+  // customize hotkeys in command palette
   // default: Control + Shift + L
   const HIDE_ON_LAUNCH = true;
   const LOCK = true;
@@ -20,18 +20,27 @@
       return newState;
     }
   };
-  const onKeyDown = (e) => {
-    // customize HOTKEYS here
-    if (e.key.toLowerCase() === "l" && e.ctrlKey && e.shiftKey) {
-      lastState = toggleLinkedRefDisplay(true);
-    }
-  };
+//  const onKeyDown = (e) => {
+//    // customize HOTKEYS here
+//    if (e.key.toLowerCase() === "l" && e.ctrlKey && e.shiftKey) {
+//      lastState = toggleLinkedRefDisplay(true);
+//    }
+//  };
+  const loadCommand = () => {
+    window.roamAlphaAPI.ui
+      .commandPalette
+      .addCommand({label: 'Hide/Display linked references section', 
+               callback: () => lastState = toggleLinkedRefDisplay(true),
+               "disable-hotkey": false,
+               "default-hotkey": "ctrl-shift-l"})
+  }
   const onPageLoad = () => {
     setTimeout(() => {
       lastState = toggleLinkedRefDisplay(false);
     }, 20);
   };
   window.addEventListener("popstate", onPageLoad);
-  document.addEventListener("keydown", onKeyDown);
+  loadCommand();
+//  document.addEventListener("keydown", onKeyDown);
   if (HIDE_ON_LAUNCH) onPageLoad();
 })();
